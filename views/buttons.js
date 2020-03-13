d54fa3586a5a60a360205c6c21c62bc45565dc33
James Fawcus-Robinson <jameslmfr@gmail.com>
2020-03-13 22:15:16 +0000

    move functions around for clarity

--- views/buttons.js
+++ views/buttons.js
@@ -3,22 +3,6 @@ const aos_colour = "#d81010";
 const stock_colour = "#1999bb";
 let stock_levels = {};
 
-function updateFromLocal(button_number) {
-  const button = document.getElementById(`button_${button_number}`);
-
-  if (button.className != "aos") {
-    stock_levels[button.text] = "aos";
-    console.log(`${button_number} is out of stock`);
-    socket.emit("update table", JSON.stringify(stock_levels));
-    updateLevel('aos', button_number);
-  } else {
-    stock_levels[button.text] = "stock";
-    console.log(`${button_number} is back in stock`);
-    socket.emit("update table", JSON.stringify(stock_levels));
-    updateLevel('stock', button_number);
-  }
-}
-
 function confirmUpdate(button_number) {
   const r = confirm(`Are you sure you want to update number ${button_number}`);
   if (r != true) {
@@ -35,6 +19,22 @@ function updateLevel(stock_level, button_number) {
   } else if (stock_level == "stock") {
     button_id.className = "stock";
     button_id.style.background = stock_colour;
+  }
+}
+
+function updateFromLocal(button_number) {
+  const button = document.getElementById(`button_${button_number}`);
+
+  if (button.className != "aos") {
+    stock_levels[button.text] = "aos";
+    console.log(`${button_number} is out of stock`);
+    socket.emit("update table", JSON.stringify(stock_levels));
+    updateLevel('aos', button_number);
+  } else {
+    stock_levels[button.text] = "stock";
+    console.log(`${button_number} is back in stock`);
+    socket.emit("update table", JSON.stringify(stock_levels));
+    updateLevel('stock', button_number);
   }
 }
 
