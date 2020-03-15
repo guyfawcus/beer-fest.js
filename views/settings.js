35e16392731735a8e4844c835bef9eda64c3c841
James Fawcus-Robinson <jameslmfr@gmail.com>
2020-03-15 20:02:22 +0000

    add table clearing code

--- views/settings.js
+++ views/settings.js
@@ -1,7 +1,16 @@
 const socket = io.connect(self.location.host);
 
 function tableClear() {
-
+  const r = confirm("Are you sure you want to clear the whole table");
+  if (r != true) {
+    return;
+  }
+  console.log("Clearing table");
+  const table = {};
+  for (i = 1; i <= 80; i++) {
+    table[i] = "stock";
+  }
+  socket.emit("update table", JSON.stringify(table));
 }
 
 function tableUpload() {
