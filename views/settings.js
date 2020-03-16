41fc59c95d6d44ba65ce4151cbc53866fff4bd66
James Fawcus-Robinson <jameslmfr@gmail.com>
2020-03-16 08:14:03 +0000

    implement download function

--- views/settings.js
+++ views/settings.js
@@ -1,4 +1,5 @@
 const socket = io.connect(self.location.host);
+let stock_levels = {};
 
 function tableClear() {
   const r = confirm("Are you sure you want to clear the whole table");
@@ -18,5 +19,19 @@ function tableUpload() {
 }
 
 function tableDownload() {
-
+  let file = new Blob([JSON.stringify(stock_levels)], {type: "application/json;charset=utf-8"});
+  let download_url = URL.createObjectURL(file);
+  let download_element = document.createElement("a");
+  download_element.style.display = "none";
+  download_element.setAttribute("href", download_url);
+  download_element.setAttribute("download", "state.json");
+  document.body.appendChild(download_element);
+  download_element.click();
+  document.body.removeChild(download_element);
 }
+
+// Update the state when remotes send updates
+socket.on("update table", table => {
+  stock_levels = JSON.parse(table);
+  console.log(`Updating table from ${table}`);
+});
