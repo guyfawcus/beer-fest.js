guyfawcus/beer-fest.js

commit 587bd93fe194cc44a7e34d9b439528eb2e466337

only update a single button unless needed

server.js
@@ -74,6 +74,15 @@ io.on("connection", socket => {
     saveState(table);
   });
 
+  socket.on("update single", stock_level => {
+    console.log(
+      `Distibuting updates from ${socket.id} (number ${stock_level[0]} = ${stock_level[1]})`
+    );
+    last_table[stock_level[0]] = stock_level[1];
+    io.sockets.emit("update single", stock_level);
+    saveState(JSON.stringify(last_table));
+  });
+
   socket.on("disconnect", () => {
     console.log(`[ server.js ] ${socket.id} disconnected`);
   });

views/buttons.js
@@ -30,13 +30,11 @@ function updateFromLocal(button_number) {
 
   if (button.className != "aos") {
     stock_levels[button_number] = "aos";
-    console.log(`${button_number} is out of stock`);
-    socket.emit("update table", JSON.stringify(stock_levels));
+    socket.emit("update single", [button_number, "aos"]);
     updateLevel("aos", button_number);
   } else {
     stock_levels[button_number] = "stock";
-    console.log(`${button_number} is back in stock`);
-    socket.emit("update table", JSON.stringify(stock_levels));
+    socket.emit("update single", [button_number, "stock"]);
     updateLevel("stock", button_number);
   }
 }
@@ -57,6 +55,12 @@ socket.on("update table", table => {
   stock_levels = JSON.parse(table);
   updateFromState(stock_levels);
   console.log(`Updating table from ${table}`);
+});
+
+socket.on("update single", stock_level => {
+  stock_levels[stock_level[0]] = stock_level[1];
+  updateLevel(stock_level[1], stock_level[0]);
+  console.log(`Number ${stock_level[0]} = ${stock_level[1]}`);
 });
 
 socket.on("connect", () => {

commit a9931c11068438c51ddf0ecb5432f6651856e875

clean up and improve messaging code
This also fixes a bug introduced in:
587bd93fe194cc44a7e34d9b439528eb2e466337

which broke the state downloading functionality
because the state wasn't being updated on each 'update single'

server.js
@@ -79,19 +79,19 @@ app.get("/js/reveal.js", (req, res) => {
 io.on("connection", socket => {
   console.log(`[ server.js ] ${socket.id} connected`);
   console.log("Distibuting previous state");
-  io.sockets.emit("update table", JSON.stringify(last_table));
+  io.to(`${socket.id}`).emit("update table", JSON.stringify(last_table));
 
   socket.on("update table", table => {
     console.log(`Distibuting updates from ${socket.id}`);
     last_table = JSON.parse(table);
-    io.sockets.emit("update table", table);
+    socket.broadcast.emit("update table", table);
     saveState(table);
   });
 
   socket.on("update single", stock_level => {
     console.log(`Distibuting updates from ${socket.id} (number ${stock_level[0]} = ${stock_level[1]})`);
     last_table[stock_level[0]] = stock_level[1];
-    io.sockets.emit("update single", stock_level);
+    socket.broadcast.emit("update single", stock_level);
     saveState(JSON.stringify(last_table));
   });
 

views/buttons.js
@@ -16,9 +16,13 @@ function confirmUpdate(button_number) {
 function updateLevel(button_number, stock_level) {
   const button_id = document.getElementById(`button_${button_number}`);
   if (stock_level == "aos") {
+    console.log(`Setting ${button_number} as out-of-stock`);
+    stock_levels[button_number] = "aos";
     button_id.className = "aos";
     button_id.style.background = aos_colour;
   } else if (stock_level == "stock") {
+    console.log(`Setting ${button_number} as in-stock`);
+    stock_levels[button_number] = "stock";
     button_id.className = "stock";
     button_id.style.background = stock_colour;
   }
@@ -29,11 +33,9 @@ function updateFromLocal(button_number) {
   const button_id = document.getElementById(`button_${button_number}`);
 
   if (button_id.className != "aos") {
-    stock_levels[button_number] = "aos";
     socket.emit("update single", [button_number, "aos"]);
     updateLevel(button_number, "aos");
   } else {
-    stock_levels[button_number] = "stock";
     socket.emit("update single", [button_number, "stock"]);
     updateLevel(button_number, "stock");
   }
@@ -52,15 +54,12 @@ function updateFromState(stock_levels) {
 
 // Update the state when remotes send updates
 socket.on("update table", table => {
-  stock_levels = JSON.parse(table);
-  updateFromState(stock_levels);
+  updateFromState(JSON.parse(table));
   console.log(`Updating table from ${table}`);
 });
 
 socket.on("update single", stock_level => {
-  stock_levels[stock_level[0]] = stock_level[1];
   updateLevel(stock_level[0], stock_level[1]);
-  console.log(`Number ${stock_level[0]} = ${stock_level[1]}`);
 });
 
 socket.on("connect", () => {

views/settings.js
@@ -87,6 +87,10 @@ socket.on("update table", table => {
   console.log(`Updating table from ${table}`);
 });
 
+socket.on("update single", stock_level => {
+  stock_levels[stock_level[0]] = stock_level[1];
+});
+
 socket.on("connect", () => {
   console.log("Server connected");
   document.getElementById("title").style.color = "#f5f6fa";