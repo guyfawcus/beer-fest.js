guyfawcus/beer-fest.js

commit 7af6edf56cef3a7373fcfdcdac34246b293e4a5d

handle 404 and 500 errors

server.js
@@ -353,6 +353,9 @@ function checkNotAuthenticated(req, res, next) {
 // ---------------------------------------------------------------------------
 // Routes - main
 // ---------------------------------------------------------------------------
+// Public files
+app.use(express.static('public'))
+
 // Core pages
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'))
@@ -432,8 +435,6 @@ app.get('/js/reveal.esm.js', (req, res) => {
 app.get('/js/reveal.esm.js.map', (req, res) => {
   res.sendFile(path.join(__dirname, 'node_modules/reveal.js/dist/reveal.esm.js.map'))
 })
-
-app.use(express.static('public'))
 
 // ---------------------------------------------------------------------------
 // Routes - authentication
@@ -529,6 +530,20 @@ app.post('/api/stock_levels/:number/:level', (req, res) => {
     console.log('API use is not enabled')
     res.status(403).send('API use is not enabled')
   }
+})
+
+// ---------------------------------------------------------------------------
+// Routes - errors
+// ---------------------------------------------------------------------------
+// Handle 404
+app.use(function (req, res) {
+  res.sendFile(path.join(__dirname, 'views/misc/404.html'), 404)
+})
+
+// Handle 500
+app.use(function (error, req, res, next) {
+  res.sendFile(path.join(__dirname, 'views/misc/500.html'), 500)
+  console.log('Server error:', error)
 })
 
 // ---------------------------------------------------------------------------

views/misc/404.html
@@ -1,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en-GB">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=0.6" />
+    <meta name="description" content="Bridge app to connect with QLC+ and send updates" />
+    <title>ACBF 404 Error</title>
+
+    <link href="style.css" type="text/css" rel="stylesheet" />
+    <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png" />
+    <link rel="manifest" href="manifest.json" />
+  </head>
+  <body>
+    <div class="settings_container">
+      <div class="settings_header"><h1>404: Page not Found</h1></div>
+      <div class="settings_utils">
+        <a class="button" href="/" title="Homepage">Homepage</a>
+      </div>
+    </div>
+    <footer>
+      Made with 🍺 - © 2020 guyfawcus - (<a href="https://github.com/guyfawcus/beer-fest.js">MIT Licensed</a>)
+    </footer>
+  </body>
+</html>

views/misc/500.html
@@ -1,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en-GB">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=0.6" />
+    <meta name="description" content="Bridge app to connect with QLC+ and send updates" />
+    <title>ACBF 500 Error</title>
+
+    <link href="style.css" type="text/css" rel="stylesheet" />
+    <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png" />
+    <link rel="manifest" href="manifest.json" />
+  </head>
+  <body>
+    <div class="settings_container">
+      <div class="settings_header"><h1>500: Internal Server Error</h1></div>
+      <div class="settings_utils">
+        <a class="button" href="/" title="Homepage">Homepage</a>
+      </div>
+    </div>
+    <footer>
+      Made with 🍺 - © 2020 guyfawcus - (<a href="https://github.com/guyfawcus/beer-fest.js">MIT Licensed</a>)
+    </footer>
+  </body>
+</html>

commit 0543edf9db13915691e715d5f78793850e98019b

fix pathname parsing
Worked out a bug that was somewhat fixed in
4e0c1084fcda6dcb9ea2cd4e15d263998e96e303

The 'bug' was introduced in
7af6edf56cef3a7373fcfdcdac34246b293e4a5d

The express.static middleware was moved to the top of the stack
and because there is a folder (downloads) with the same name as a page
it 'broke' it.

This fix moves the middleware back down the stack and also fixes
some other small issues that could have reared thier heads at some
point:
  * The root page `/` is given the name `index` instead of it being `''`
  * The trailing `/` is removed if present after the pathname is parsed

server.js
@@ -378,9 +378,6 @@ function checkNotAuthenticated(req, res, next) {
 // ---------------------------------------------------------------------------
 // Routes - main
 // ---------------------------------------------------------------------------
-// Public files
-app.use(express.static('public'))
-
 // Core pages
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'))
@@ -558,8 +555,11 @@ app.post('/api/stock_levels/:number/:level', (req, res) => {
 })
 
 // ---------------------------------------------------------------------------
-// Routes - errors
+// Routes - others
 // ---------------------------------------------------------------------------
+// Public files
+app.use(express.static('public'))
+
 // Handle 404
 app.use(function (req, res) {
   res.sendFile(path.join(__dirname, 'views/misc/404.html'), 404)
@@ -597,6 +597,9 @@ io.on('connection', (socket) => {
     // If there is no header information, it's likely originated from a bot
     pathname = 'bot'
   }
+
+  if (pathname === '') pathname = 'index'
+  if (pathname.slice(-1) === '/') pathname = pathname.slice(0, -1)
 
   // When a new client connects, update them with the current state of things
   console.log(`Client ${socket.id} connected (${pathname})`)