guyfawcus/beer-fest.js

commit 99b7bc5fe531af6db698e5c659dae1073d27246f

add TLS settings for redis connection
https://devcenter.heroku.com/articles/securing-heroku-redis#using-node-js

server.js
@@ -95,7 +95,10 @@ let brewery_geojson = ''
 // Initial setup
 // ---------------------------------------------------------------------------
 const app = express()
-const redisClient = redis.createClient({ url: REDIS_URL })
+const redisClient = redis.createClient({
+  url: REDIS_URL,
+  tls: { rejectUnauthorized: false, requestCert: true, agent: false }
+})
 const RedisStore = connectRedis(session)
 
 const wdk = WBK({

commit ee115b67d30f64ce7bb98b01a25dbbad7d055b3e

only use TLS for redis if in production
Fixes a bug that broke local deployments that was introduced in:
99b7bc5fe531af6db698e5c659dae1073d27246f

server.js
@@ -95,10 +95,9 @@ let brewery_geojson = ''
 // Initial setup
 // ---------------------------------------------------------------------------
 const app = express()
-const redisClient = redis.createClient({
-  url: REDIS_URL,
-  tls: { rejectUnauthorized: false, requestCert: true, agent: false }
-})
+
+const tls_config = NODE_ENV === 'production' ? { rejectUnauthorized: false, requestCert: true, agent: false } : undefined
+const redisClient = redis.createClient({ url: REDIS_URL, tls: tls_config })
 const RedisStore = connectRedis(session)
 
 const wdk = WBK({