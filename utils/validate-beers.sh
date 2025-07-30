970c14632ba9a0fa99c0fa1e962b3f0854a13f2d
James Fawcus-Robinson <jameslmfr@gmail.com>
2025-07-30 12:32:46 +0700

    improve frictionless validation
    Allow beer that has an ABV 0f 6.66%,
    and limit the file criteria in validate-beers.sh

--- utils/validate-beers.sh
+++ utils/validate-beers.sh
@@ -6,12 +6,14 @@ cd "$parent_path"/../public/downloads/
 
 for file in *.csv
 do
-  output=$(frictionless validate "$file" --schema tableschema.json 2>&1)
-  status=$?
-  if [ $status -eq 1 ]; then
-    echo "$output"
-  else
-    echo "Working on $file"
+  if [[ "$file" =~ ^ACBF[0-9]+-[0-9]{4}\.csv$ ]]; then
+    output=$(frictionless validate "$file" --schema tableschema.json 2>&1)
+    status=$?
+    if [ $status -eq 1 ]; then
+      echo "$output"
+    else
+      echo "Working on $file"
+    fi
   fi
 done
 
