e5f64a6991eceac835e14fffc4c9b254fad75266
James Fawcus-Robinson <jameslmfr@gmail.com>
2026-08-07 15:06:00 +0100

    use pipx run when validating beers

--- utils/validate-beers.sh
+++ utils/validate-beers.sh
@@ -7,7 +7,7 @@ cd "$parent_path"/../public/downloads/
 for file in *.csv
 do
   if [[ "$file" =~ ^ACBF[0-9]+-[0-9]{4}\.csv$ ]]; then
-    output=$(frictionless validate "$file" --schema tableschema.json 2>&1)
+    output=$(pipx run frictionless validate "$file" --schema tableschema.json 2>&1)
     status=$?
     if [ $status -eq 1 ]; then
       echo "$output"
